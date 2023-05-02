a60a5f5c1c4a0ac0c31c9813eac050fa58dac557
Funkinsz <fl.crespel@gmail.com>
2023-05-02 07:10:28 +0200

    fix : start profile

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,17 +1,26 @@
-import { useContext } from 'react';
-import styles from './Profile.module.scss';
-import { AuthContext } from '../../context/AuthContext';
+import { useContext } from "react";
+import styles from "./Profile.module.scss";
+import { AuthContext } from "../../context/AuthContext";
+import pp from "../../assets/image/Img register/pro4.jpeg";
 
 export default function Profile() {
-    const { user } = useContext(AuthContext)
-    return(
-        <div className={`flex-fill d-flex justify-content-center align-items-center`}>
-            <div className={`${styles.profileContainer} card p20`}>
-                <ul>
-                    <li>Nom : { user.name }</li>
-                    <li>Email : { user.email }</li>
-                </ul>
+  const { user } = useContext(AuthContext);
+
+  console.log(user);
+  return (
+    <section className={`flex-fill d-flex jcc p30`}>
+      <div className={`${styles.profileContainer} d-flex jcc`}>
+        <div className={`${styles.profileHeader} d-flex flex-fill`}>
+          <div className={`${styles.banner} d-flex jcsb`}>
+            <div className={`${styles.pp} d-flex aic jcc`}>
+              <img src={pp} alt="" />
             </div>
+            <img src={pp} alt="" />
+            <h1>{user.surname}</h1>
+            <div>icon messenger</div>
+          </div>
         </div>
-    )
+      </div>
+    </section>
+  );
 }
