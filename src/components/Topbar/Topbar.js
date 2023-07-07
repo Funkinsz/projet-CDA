2d08152b3157112fc0909a189bb51a041953ca72
Funkinsz <fl.crespel@gmail.com>
2023-07-07 15:21:29 +0200

    responsive topbar

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,26 +1,28 @@
-import { NavLink } from 'react-router-dom'
-import styles from './Topbar.module.scss'
+import { NavLink } from "react-router-dom";
+import styles from "./Topbar.module.scss";
 
 export default function Topbar() {
-    return (
-        <div className={`${styles.topbar} d-flex jcsa`}>
-            <div className={`${styles.topbarbtn} d-flex aic jcc`}>
-                <NavLink className={"d-flex aic jcc"} to = "/pros">
-                    <h3>Organisateurs</h3>
-                </NavLink>
-            </div>
-
-            <div className={`${styles.topbarbtn} ${styles.mid} d-flex aic jcc`}>
-                <NavLink className={"d-flex aic jcc"} to = "/bands">
-                    <h3>Groupes</h3>
-                </NavLink>                
-            </div>
-
-            <div className={`${styles.topbarbtn} d-flex aic jcc`}>
-                <NavLink className={"d-flex aic jcc"} to = "/arts">
-                    <h3>Artistes</h3>
-                </NavLink>
-            </div>
+  return (
+    <div className={`${styles.topbar}`}>
+      <div className={`${styles.width} d-flex jcsb`}>
+        <div className={`${styles.topbarbtn} d-flex aic jcc`}>
+          <NavLink className={"d-flex aic jcc"} to="/pros">
+            <h3>Organisateurs</h3>
+          </NavLink>
         </div>
-    )
+
+        <div className={`${styles.topbarbtn} ${styles.mid} d-flex aic jcc`}>
+          <NavLink className={"d-flex aic jcc"} to="/bands">
+            <h3>Groupes</h3>
+          </NavLink>
+        </div>
+
+        <div className={`${styles.topbarbtn} d-flex aic jcc`}>
+          <NavLink className={"d-flex aic jcc"} to="/arts">
+            <h3>Artistes</h3>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
