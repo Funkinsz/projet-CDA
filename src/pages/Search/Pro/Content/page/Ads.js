8fbe7ebbb45d58dff9921995f41e043c25234766
Funkinsz <fl.crespel@gmail.com>
2023-07-21 16:24:22 +0200

    feat: read user + city + date for ad_pro

--- src/pages/Search/Pro/Content/page/Ads.js
+++ src/pages/Search/Pro/Content/page/Ads.js
@@ -2,6 +2,16 @@ import styles from "../ContentPro.module.scss";
 import image from "../../../../../assets/image/affiche_concert.jpg";
 
 export default function Ads({ ad, mobileWidth }) {
+  const date = new Date(ad.date);
+
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const formattedDate = date.toLocaleString("fr-FR", options);
+
   return (
     <div className={`${styles.groupcontent} groupcontent d-flex jcsb`}>
       <div>
@@ -9,19 +19,16 @@ export default function Ads({ ad, mobileWidth }) {
           <img src={image} alt="" />
           <span className={`${styles.price}`}>{ad.price_ad_pro} €</span>
         </div>
-        <div className="d-flex aic jcc">
-          <span className={`${styles.divider}`}></span>
-        </div>
-        <ul
-          style={{ display: mobileWidth ? "" : "none" }}
-          className={`${styles.sono} d-flex flex-column jce`}>
-          <li>musicien : {ad.number_art}</li>
-          <li>{ad.sono == true ? "Sono : ✔️" : "Sono : ❌"}</li>
-        </ul>
       </div>
 
-      <div className={`${styles.desc} d-flex flex-column p10`}>
-        <h2>{ad.title_ad_pro}</h2>
+      <div className={`${styles.desc} d-flex flex-column jcsb p10`}>
+        <div className={`${styles.head}`}>
+          <h2>{ad.title_ad_pro}</h2>
+          <h4>
+            {ad.surname} à {ad.city.toUpperCase()}
+          </h4>
+          <h5>{formattedDate}</h5>
+        </div>
         <span>{ad.content_ad_pro}</span>
       </div>
       <div
